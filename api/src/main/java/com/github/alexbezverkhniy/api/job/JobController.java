3c9ee642ca89c918805ed5193e4301abccb17379
Alexandr Bezverkhniy <alexandr.bezverkhniy@usbank.com>
2019-08-30 16:17:38 -0500

    Added  job model and controller

---
+++ api/src/main/java/com/github/alexbezverkhniy/api/job/JobController.java
@@ -0,0 +1,28 @@
+package com.github.alexbezverkhniy.api.job;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JobController {
+
+  @Autowired
+  private JobExplorer jobExplorer;
+
+  @GetMapping("/batch/jobs")
+  public List<Job> jobs() {
+    List<String> jobNames = jobExplorer.getJobNames();
+    List<Job> result = new ArrayList<>();
+    if (jobNames != null && !jobNames.isEmpty()) {
+      result = jobNames.stream().map(name -> {
+        return new Job(name);
+      }).collect(Collectors.toList());
+    }
+    return result;
+  }
+}
