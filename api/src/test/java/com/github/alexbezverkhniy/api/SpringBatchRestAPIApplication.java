9ef07110b8a3b2d14ef9b5eddfd37d2d178c11e9
Alexandr Bezverkhniy <alexandr.bezverkhniy@usbank.com>
2019-08-30 16:16:53 -0500

    Added test for JobControlles + config

---
+++ api/src/test/java/com/github/alexbezverkhniy/api/SpringBatchRestAPIApplication.java
@@ -0,0 +1,13 @@
+package com.github.alexbezverkhniy.api;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class SpringBatchRestAPIApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBatchRestAPIApplication.class, args);
+  }
+}
---
+++ api/src/test/java/com/github/alexbezverkhniy/api/job/JobControllerTest.java
@@ -0,0 +1,66 @@
+package com.github.alexbezverkhniy.api.job;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class JobControllerTest {
+  public static final String JOB_NAME = "jobName";
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @MockBean
+  private JobExplorer jobExplorer;
+
+  @Test
+  public void jobs() throws Exception {
+    List<Job> want = Arrays.asList(new Job(JOB_NAME));
+
+    when(jobExplorer.getJobNames()).thenReturn(Arrays.asList(JOB_NAME));
+
+    mockMvc.perform(get("/batch/jobs").contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().json(objectMapper.writeValueAsString(want)));
+
+    // Empty list
+    when(jobExplorer.getJobNames()).thenReturn(null);
+
+    mockMvc.perform(get("/batch/jobs").contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().json(objectMapper.writeValueAsString(new ArrayList<Job>())));
+
+    // Empty list
+    when(jobExplorer.getJobNames()).thenReturn(new ArrayList<String>());
+
+    mockMvc.perform(get("/batch/jobs").contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().json(objectMapper.writeValueAsString(new ArrayList<Job>())));
+
+
+    verify(jobExplorer, times(3)).getJobNames();
+  }
+
+}
